705a650df9d5b61f263739f7b2b70597698660ae
Eduardo D Mori <emori1968@gmail.com>
2024-08-19 17:21:59 -0300

    2.8 Phonebook step3

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,29 +2,45 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas',
+      number: '39-44-5323523' }
   ]) 
   const [newName, setNewName] = useState('')
+  const  [newNumber,setNewNumber] = useState('')
+
 
   const addPerson = (event) => {
     event.preventDefault()
     console.log('botton clicked', event.target)
     // person is and Object {name: "Eduardo"}
     const personObject = {
-      name: newName
+      name: newName,
+      number: newNumber
     }
     // now new person can be added to the list
     setPersons(persons.concat(personObject))
     // reset previos value
     setNewName('')
+    setNewNumber('')
   }
 
-  const handlePersonChange = (event) => {
+  const handleNameChange = (event) => {
     console.log(event.target.value)
-    setNewName(event.target.value)
+    setNewName(event.target.value);
+    }
+    
+  // No name repeat check. Not working well!!
+  const isNew = persons.find((person) => person.name === newName )
+  if ( isNew != undefined ) {
+    alert(`${newName} is already on the phonebook`);
   }
 
+  const handleNumberChange = (event) => {
+    console.log(event.target.value)
+    setNewNumber(event.target.value);
+    }
 
+  
   return (
     <div>
       <h2>Phonebook</h2>
@@ -32,16 +48,24 @@ const App = () => {
         <div>
           name: <input
                    value = {newName}
-                   onChange= {handlePersonChange}
+                   onChange= {handleNameChange}
                 />
         </div>
+        <div>
+          number: <input
+                   value = {newNumber}
+                   onChange= {handleNumberChange}
+                />
+        </div>
+
+
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map(row => <p key={row.name}>{row.name}</p>)}
+        {persons.map(row => <p key={row.name}>{row.name} {row.number}</p>)}
       </ul>
     </div>
   )
