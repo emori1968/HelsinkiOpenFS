84b472793b210b2e70683a695f9c9a83109856f6
Eduardo D Mori <emori1968@gmail.com>
2024-08-19 19:38:13 -0300

    2.9 Phonebook step4

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,12 +1,17 @@
 import { useState } from 'react'
 
 const App = () => {
+
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas',
-      number: '39-44-5323523' }
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
+
   const [newName, setNewName] = useState('')
   const  [newNumber,setNewNumber] = useState('')
+  const [filterName, setFilterName] = useState('')
 
 
   const addPerson = (event) => {
@@ -15,7 +20,8 @@ const App = () => {
     // person is and Object {name: "Eduardo"}
     const personObject = {
       name: newName,
-      number: newNumber
+      number: newNumber,
+      id: persons.length +1,
     }
     // now new person can be added to the list
     setPersons(persons.concat(personObject))
@@ -30,9 +36,11 @@ const App = () => {
     }
     
   // No name repeat check. Not working well!!
-  const isNew = persons.find((person) => person.name === newName )
-  if ( isNew != undefined ) {
-    alert(`${newName} is already on the phonebook`);
+  if (newName != '') {
+    const isNew = persons.find((person) => person.name === newName )
+    if ( isNew != undefined ) {
+      alert(`${newName} is already on the phonebook`);
+    }
   }
 
   const handleNumberChange = (event) => {
@@ -40,10 +48,29 @@ const App = () => {
     setNewNumber(event.target.value);
     }
 
+  const handleFilterChange = (event) => {
+    console.log(filterName)
+    setFilterName(event.target.value)
+  }
+
+  // filter person list 
+  const filteredlist =  (filterName != '')
+  ? persons.filter(row => row.name === filterName)
+  : persons
+
+
   
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+          filter shown with: <input
+                                value = {filterName}
+                                onChange= {handleFilterChange}
+                              />
+        </div>
+
+      <h2>add a new</h2>
       <form onSubmit= {addPerson}>
         <div>
           name: <input
@@ -65,7 +92,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map(row => <p key={row.name}>{row.name} {row.number}</p>)}
+        {filteredlist.map(row => <p key={row.id}>{row.name} {row.number}</p>)}
       </ul>
     </div>
   )
