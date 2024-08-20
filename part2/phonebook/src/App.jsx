715ef1b2e6169fe5e4462a578dc686fbf7c5f050
Eduardo D Mori <emori1968@gmail.com>
2024-08-20 16:52:28 -0300

    2.13 Phonebook step8

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
+import personService from './services/persons'
 
 
 const Filter = (props) => {
@@ -60,17 +61,13 @@ const App = () => {
 
   useEffect(() => {
     console.log('effect')
-    axios
-      .get('http://localhost:3001/persons')
-      .then(response => {
-        console.log('promise fulfilled')
-        setPersons(response.data)
+    personService
+      .getAll()
+      .then(initialPersons => {
+        setPersons(initialPersons)
       })
   }, [])
   console.log('render', persons.length, 'persons')
-
-
-
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -79,12 +76,11 @@ const App = () => {
     const personObject = {
       name: newName,
       number: newNumber,
-      id: persons.length +1,
     }
-    axios
-      .post('http://localhost:3001/persons', personObject)
-      .then(response => {
-        setPersons(persons.concat(response.data))
+    personService
+      .create(personObject)
+      .then(returnedPerson => {
+        setPersons(persons.concat(returnedPerson))
         setNewName('')
         setNewNumber('')
       })
