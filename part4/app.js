fc30ca492ebdc2c87637c0245039363929b6ae61
Eduardo D Mori <emori1968@gmail.com>
2024-09-04 13:48:27 -0300

    4.2 Blogs List step2 All factorized

---
+++ part4/app.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const app = express()
+
+
+// for front end running in at different address
+const cors = require('cors')
+
+
+const middleware = require('./utils/middleware')
+
+const blogsRouter = require('./controllers/blogs')
+
+const logger = require('./utils/logger')
+const config = require('./utils/config')
+
+const mongoose = require('mongoose')
+
+mongoose.set('strictQuery', false)
+
+mongoose.connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connecting to MongoDB:', error.message)
+  })
+
+  // front end compilation
+app.use(cors())
+app.use(express.static('dist'))
+
+app.use(express.json())  // not working to show body at cli
+app.use(middleware.requestLogger)
+
+// get,post added to app
+app.use('/api/blogs', blogsRouter)
+
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+
+module.exports = app
--- part4/index.js
+++ part4/index.js
@@ -1,55 +1,6 @@
-const express = require('express')
-const app = express()
-app.use(express.json())
-const cors = require('cors')
-app.use(cors())
-
-const mongoose = require('mongoose')
-
-// const app = require('./app') // la aplicación Express real
+const app = require('./app')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
-
-const Blog = require('./models/blog')
-
-// const Blog = mongoose.model('Blog', blogSchema)
-
-mongoose.connect(config.MONGODB_URI)
-
-// now routers are in controllers/blog modules
-const blogsRouter = require('./controllers/blogs')
-const { requestLogger } = require('./utils/middleware')
-
-// here blogsRouter receive the path, router only connects from root  "/"   or "/id:"
-app.use('/api/blogs', blogsRouter)
-
-//Adding middleware
-const middleware = require('./utils/middleware')
-app.use(middleware.requestLogger)
-app.use(middleware.unknownEndpoint)
-app.use(middleware.errorHandler)
-
-/*  No more necesary, the magic "use" method make it available from app. I think this is "una cagada"
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-*/
-
 
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
