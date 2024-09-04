ce3358b490f80500f16dbd801ebc017d42178b06
Eduardo D Mori <emori1968@gmail.com>
2024-09-04 11:07:20 -0300

    4.2 version with factorization of models, logger,config

--- part4/index.js
+++ part4/index.js
@@ -3,17 +3,15 @@ const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
+// const app = require('./app') // la aplicación Express real
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-const Blog = mongoose.model('Blog', blogSchema)
+const Blog = require('./models/blog')
 
-const mongoUrl = 'mongodb+srv://emori1968:uVGpNNanwkkl8Qnd@cluster0.it4ey.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
-mongoose.connect(mongoUrl)
+// const Blog = mongoose.model('Blog', blogSchema)
+
+mongoose.connect(config.MONGODB_URI)
 
 app.use(cors())
 app.use(express.json())
@@ -36,7 +34,7 @@ app.post('/api/blogs', (request, response) => {
     })
 })
 
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
