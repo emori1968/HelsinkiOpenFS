86d6a121c7350f216b73fc0812f4168be20055d6
Eduardo D Mori <emori1968@gmail.com>
2024-09-26 15:56:46 -0300

    5.7 Blog List Frontend, step 7

--- part5/src/App.jsx
+++ part5/src/App.jsx
@@ -110,7 +110,7 @@ return (
               </div>
     }
     <ul>
-      {blogsToShow.map(blog => 
+      {blogsToShow.map(blog =>
         <Blog
           key={blog.id}
           blog={blog}
--- part5/src/components/Blog.jsx
+++ part5/src/components/Blog.jsx
@@ -1,7 +1,31 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import Togglable from "./Togglable"
+let showme = false
+
+const showhandler = (event) => {
+ showme = !showme
+}
+
+const Blog = ({ blog }) => {
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+
+  }
+  return(
+    <div style={blogStyle}>
+      <div>
+          {blog.title}
+          <Togglable buttonLabel= 'view'>
+            {blog.url}<br/>
+            Likes: {blog.likes} <button>like</button>  <br/>
+            {blog.author} <br/>
+          </Togglable>
+      </div>
+    </div>
+)}
 
 export default Blog
