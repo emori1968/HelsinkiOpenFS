fb88087601c92790911d997798bcfbb2ae11a623
Eduardo D Mori <emori1968@gmail.com>
2024-10-07 13:33:45 -0300

    5.19: Blog List End To End Testing, step 3

--- playwright/e2e/login_app.test.js
+++ playwright/e2e/login_app.test.js
@@ -1,6 +1,8 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test')
+const { createBlog } = require('./helper')
 
 describe('Blog app', () => {
+
   beforeEach(async ({ page, request }) => {
     await request.post('http://localhost:3003/api/testing/reset')
     await request.post('http://localhost:3003/api/users', {
@@ -20,7 +22,9 @@ describe('Blog app', () => {
   })
 
   describe('Login', () => {
+
     test('succeeds with correct credentials', async ({ page }) => {
+
         await page.goto('http://localhost:5173')
         await page.getByRole('button', { name: 'log in' }).click()
         await page.getByRole('textbox').first().fill('jacinto')
@@ -29,12 +33,30 @@ describe('Blog app', () => {
         await expect(page.getByText('vergara logged in')).toBeVisible()  })
 
     test('fails with wrong credentials', async ({ page }) => {
+
         await page.goto('http://localhost:5173')
         await page.getByRole('button', { name: 'log in' }).click()
         await page.getByRole('textbox').first().fill('jacinto')
-        await page.getByRole('textbox').last().fill('ramos')
+        await page.getByRole('textbox').last().fill('ramose')
         await page.getByRole('button', { name: 'login' }).click()
         await expect(page.getByText('username')).toBeVisible()  })
     })
+
+
+  
+  describe('when logged in', () => {
+
+    beforeEach(async ({ page }) => {
+      await page.getByRole('button', { name: 'log in' }).click()
+      await page.getByTestId('username').first('jacinto')
+      await page.getByTestId('password').last('ramos')
+      await page.getByRole('button', { name: 'login' }).click()
+    })
+    
+    test('a new blog can be created', async ({ page }) => {
+      await createBlog(page, 'a title created by playwright', 'author fill in', 'url fill in',true )
+      await expect(page.getByText('a title created by playwright')).toBeVisible()
+    })  
+  })
 
   })
